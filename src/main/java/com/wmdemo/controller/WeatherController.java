b1f106926edc8a643db2947267f189f30d19035e
Eugen Barbu <liviu.barbu@ro.ibm.com>
2020-11-27 10:25:26 +0200

    EB: Updating README.md

--- src/main/java/com/wmdemo/controller/WeatherController.java
+++ src/main/java/com/wmdemo/controller/WeatherController.java
@@ -43,7 +43,7 @@ public class WeatherController {
     }
 
 
-    double random(double min, double max) {
+    private double random(double min, double max) {
         Random r = new Random();
         return (r.nextInt((int)((max-min)*10+1))+min*10) / 10.0;
     }
